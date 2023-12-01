426483b07257b49c3d811739b42ea0f767f5cae2
rneeser <rebecca.neeser@epfl.ch>
2023-12-01 17:51:00 +0100

    feat: data preprocessing

--- src/cyc_pep_perm/data/processing.py
+++ src/cyc_pep_perm/data/processing.py
@@ -1,10 +1,26 @@
 import os
+import pickle
 
 import pandas as pd
 from pandas_ods_reader import read_ods
 import mordred
 from mordred import Calculator, descriptors
 from rdkit import Chem
+from sklearn.preprocessing import StandardScaler
+
+
+FEATURES_DW = [
+    "MW",
+    "cLogP",
+    "cLogS",
+    "HBA",
+    "HBD",
+    "Total Surface Area",
+    "Rel. PSA",
+    "PSA",
+    "Rot. Bonds",
+    "Amides",
+]
 
 
 class DataProcessing:
@@ -14,7 +30,7 @@ class DataProcessing:
 
     def __init__(
         self,
-        datapath="data/raw/Cyclic_peptide_membrane_permeability_random80percent.ods",
+        datapath="data/Cyclic_peptide_membrane_permeability_random80percent.ods",
         target_label="CAPA [1 µM]",
         smiles_label="SMILES",
     ):
@@ -31,9 +47,8 @@ class DataProcessing:
         self.target_label = target_label
         self.smiles_label = smiles_label
         self.data = None
+        self.df_mordred = None
         self.calculator = Calculator(descriptors, ignore_3D=True)
-
-        self.read_data()
 
     def read_data(self):
         """
@@ -47,13 +62,19 @@ class DataProcessing:
         self.smiles = self.data[self.smiles_label]
         self.mols = [Chem.MolFromSmiles(smile) for smile in self.smiles]
 
-        self.data.rename(columns={self.target_label: "target"}, inplace=True)
+        if self.target_label in self.data.columns:
+            self.data.rename(columns={self.target_label: "target"}, inplace=True)
         self.data.rename(columns={self.smiles_label: "SMILES"}, inplace=True)
 
-        self.data.to_csv(
-            f"data/raw/{self.datapath.split('/')[-1].split('.')[0]}.csv",
-            index=False,
-        )
+        # drop irrelevant columns for training
+        self.data = self.data[['SMILES', 'target'] + FEATURES_DW]
+
+        new_filepath = f"data/{self.datapath.split('/')[-1].split('.')[0]}.csv"
+        self.data.to_csv(new_filepath, index=False)
+
+        print(f"Saved data to {new_filepath}")
+
+        return self.data
 
     def calc_mordred(self, filename=None):
         """
@@ -63,26 +84,149 @@ class DataProcessing:
             filename (str, optional): Path to save the Mordred data. If not provided, a
             default filename will be used.
         """
-        df_mordred = self.calculator.pandas(self.mols)
-        for col in df_mordred.columns:
-            vals = df_mordred[col].values
+        if self.data is None:
+            self.read_data()
+
+        self.df_mordred = self.calculator.pandas(self.mols)
+        for col in self.df_mordred.columns:
+            vals = self.df_mordred[col].values
             if all(isinstance(x, mordred.error.Error) for x in vals):
-                df_mordred = df_mordred.drop(col, axis=1)
+                self.df_mordred = self.df_mordred.drop(col, axis=1)
 
-        df_mordred["SMILES"] = self.smiles
-        df_mordred["target"] = self.data[self.target_label]
+        assert len(self.df_mordred.columns) == 1597
 
-        assert len(df_mordred.columns) == 1599
+        self.df_mordred["SMILES"] = self.smiles
+        if "target" in self.data.columns:
+            self.df_mordred["target"] = self.data['target']
 
         if not filename:
-            filename = "data/raw/"\
-                  f'{self.datapath.split("/")[-1].split(".")[0]}_mordred.csv'
-        df_mordred.to_csv(filename, index=False)
+            filename = "data/" \
+                f'{self.datapath.split("/")[-1].split(".")[0]}_mordred.csv'
+        self.df_mordred.to_csv(filename, index=False)
 
-    def scale_data(self):
+        print(f"Saved Mordred descriptors to {filename}")
+
+        return self.df_mordred
+
+    def scale_train_data(self, mordred=False, raw_data=None):
         """
-        Scale the original and Mordred data.
+        Scale the training set of the original (DataWarrior descs) or Mordred data. Not
+        needed for tree-based models (RF, XGBoost).
+
+        Args:
+            mordred (bool, optional): Whether to scale the Mordred data. Defaults to
+            False.
+            raw_data (str, optional): Path to the raw data file. If not provided,
+            the default path will be used.
         """
-        # TODO: Implement scaling for both original and Mordred data
         os.makedirs("data/scaled", exist_ok=True)
-        pass
+
+        if raw_data:
+            data = pd.read_csv(raw_data)
+            scaler_path = raw_data.split('.')[0] + '_scaler.pkl'
+        else:
+            scaler_path = self.datapath.split('.')[0] + '_scaler.pkl'
+            raw_data = self.datapath.split('.')[0] + '.csv'
+            if mordred:
+                raw_data = raw_data.split('.')[0] + '_mordred.csv'
+                assert self.df_mordred is not None, 'Mordred data not loaded'
+                data = self.df_mordred
+            else:
+                assert self.data is not None, 'Data not loaded'
+                data = self.data
+
+        if mordred:
+            scaler_path = scaler_path.split('.')[0] + '_mordred.pkl'
+            assert 'nAcid' in data.columns, 'Data does not contain Mordred descriptors'
+            feat_cols = [col for col in data.columns if col not in ["SMILES", "target"]]
+            # array of features
+            X = data[feat_cols].values
+        else:
+            assert set(FEATURES_DW).issubset(set(data.columns)), \
+                'Data does not contain DataWarrior descriptors'
+            assert len(set(FEATURES_DW).intersection(set(data.columns))) == 10, \
+                'Data does not contain all DataWarrior descriptors'
+            feat_cols = FEATURES_DW
+            # array of features
+            X = data[feat_cols].values
+        # array of targets
+        y = data["target"].values
+
+        y_scaled = y / 100
+
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+
+        # save scaler
+        with open(scaler_path, "wb") as f:
+            pickle.dump(scaler, f)
+
+        # save scaled data
+        df_scaled = pd.DataFrame(X_scaled, columns=feat_cols)
+        df_scaled["target"] = y_scaled
+        df_scaled["SMILES"] = data["SMILES"]
+        df_scaled.to_csv(raw_data.split('.')[0] + '_scaled.csv', index=False)
+
+        print(f"Saved scaled data to {raw_data.split('.')[0] + '_scaled.csv'}")
+        print(f"Saved scaler to {scaler_path}")
+
+    def scale_test_data(self, mordred=False, raw_data=None):
+        """
+        Scale the test set of the original (DataWarrior descs) or Mordred data.
+        Not needed for tree-based models (RF, XGBoost).
+
+        Args:
+            mordred (bool, optional): Whether to scale the Mordred data. Defaults to
+            False.
+            raw_data (str, optional): Path to the raw data file. If not provided,
+            the default path will be used.
+        """
+        os.makedirs("data/scaled", exist_ok=True)
+
+        if raw_data:
+            data = pd.read_csv(raw_data)
+            scaler_path = raw_data.split('.')[0] + '_scaler.pkl'
+        else:
+            scaler_path = self.datapath.split('.')[0] + '_scaler.pkl'
+            raw_data = self.datapath.split('.')[0] + '.csv'
+            if mordred:
+                raw_data = raw_data.split('.')[0] + '_mordred.csv'
+                assert self.df_mordred is not None, 'Mordred data not loaded'
+                data = self.df_mordred
+            else:
+                assert self.data is not None, 'Data not loaded'
+                data = self.data
+
+        if mordred:
+            scaler_path = scaler_path.split('.')[0] + '_mordred.pkl'
+            assert 'nAcid' in data.columns, 'Data does not contain Mordred descriptors'
+            feat_cols = [col for col in data.columns if col not in ["SMILES", "target"]]
+            # array of features
+            X = data[feat_cols].values
+        else:
+            assert set(FEATURES_DW).issubset(set(data.columns)), \
+                'Data does not contain DataWarrior descriptors'
+            assert len(set(FEATURES_DW).intersection(set(data.columns))) == 10, \
+                'Data does not contain all DataWarrior descriptors'
+            feat_cols = FEATURES_DW
+            # array of features
+            X = data[feat_cols].values
+        # array of targets
+        y = data["target"].values
+
+        y_scaled = y / 100
+
+        # load scaler
+        with open(scaler_path, "rb") as f:
+            scaler = pickle.load(f)
+
+        X_scaled = scaler.transform(X)
+
+        # save scaled data
+        df_scaled = pd.DataFrame(X_scaled, columns=feat_cols)
+        df_scaled["target"] = y_scaled
+        df_scaled["SMILES"] = data["SMILES"]
+        df_scaled.to_csv(raw_data.split('.')[0] + '_scaled.csv', index=False)
+
+        print(f"Saved scaled data to {raw_data.split('.')[0] + '_scaled.csv'}")
+        print(f"Saved scaler to {scaler_path}")
