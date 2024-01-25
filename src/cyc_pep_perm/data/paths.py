8c848edb703eca8ef525064929a2a40b25403aeb
rneeser <rebecca.neeser@epfl.ch>
2024-01-25 20:31:58 +0100

    add: script for polynomimal fitting

--- src/cyc_pep_perm/data/paths.py
+++ src/cyc_pep_perm/data/paths.py
@@ -12,6 +12,12 @@ TRAIN_RANDOM_DW = os.path.join(
 TRAIN_RANDOM_MORDRED = os.path.join(
     DATA_PATH, "perm_random80_train_mordred.csv"
 )
+TEST_RANDOM_DW = os.path.join(
+    DATA_PATH, "perm_random20_test_dw.csv"
+)
+TEST_RANDOM_MORDRED = os.path.join(
+    DATA_PATH, "perm_random20_test_mordred.csv"
+)
 
 MODEL_PATH = os.path.join(ROOT_PATH, "models")
 MODEL_RF_RANDOM_DW = os.path.join(MODEL_PATH, "rf_random_dw.pkl")
@@ -19,4 +25,3 @@ MODEL_RF_RANDOM_MORDRED = os.path.join(MODEL_PATH, "rf_random_mordred.pkl")
 MODEL_XGB_RANDOM_DW = os.path.join(MODEL_PATH, "xgb_random_dw.pkl")
 MODEL_XGB_RANDOM_MORDRED = os.path.join(MODEL_PATH, "xgb_random_mordred.pkl")
 MODEL_RFCLASS_RANDOM_DW = os.path.join(MODEL_PATH, "rfclass_random_dw.pkl")
-
